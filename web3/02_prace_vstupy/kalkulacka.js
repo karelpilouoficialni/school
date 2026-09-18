70abf195930bf8a7d9d82a144a821e08a7506b0a
exxo550 <naturallavendercandle@gmail.com>
2026-09-18 12:46:36 +0200

    vykalkulatorovano

---
+++ web3/02_prace_vstupy/kalkulacka.js
@@ -0,0 +1,14 @@
+//console.log("jaja test before");
+
+let num1 = document.getElementById("number1");
+let num2 = document.getElementById("number2");
+let but = document.getElementById("button");
+let res = document.getElementById("result");
+
+function calc() {
+    //console.log("calc test");
+    let total = parseInt(num1.value) + parseInt(num2.value);
+    console.log(total);
+}
+
+but.addEventListener("click", calc);
